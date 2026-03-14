57621209ceb33fb0797ecd1d10f04129e9fbbdff
Atharv Bapat <134592383+atharv020304@users.noreply.github.com>
2026-03-14 12:58:48 +0530

    Update sha1.hpp

--- parser/sha1.hpp
+++ parser/sha1.hpp
@@ -1,27 +1,6 @@
-/*
-    sha1.hpp - source code of
-
-    ============
-    SHA-1 in C++
-    ============
-
-    100% Public Domain.
-
-    Original C Code
-        -- Steve Reid <steve@edmweb.com>
-    Small changes to fit into bglibs
-        -- Bruce Guenter <bruce@untroubled.org>
-    Translation to simpler C++ Code
-        -- Volker Diels-Grabsch <v@njh.eu>
-    Safety fixes
-        -- Eugene Hopkinson <slowriot at voxelstorm dot com>
-    Header-only library
-        -- Zlatko Michailov <zlatko@michailov.org>
-*/
 
 #ifndef SHA1_HPP
 #define SHA1_HPP
-
 
 #include <cstdint>
 #include <fstream>
