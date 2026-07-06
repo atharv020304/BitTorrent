f8bc336c57a089e61496597b3924f07933e3c90f
Atharv Bapat <atharvabapat3@gmail.com>
2026-07-06 18:16:00 +0530

    utils added

---
+++ src/utils.h
@@ -0,0 +1,20 @@
+#ifndef TORRENT_UTILS_H
+#define TORRENT_UTILS_H
+
+#include<bits/stdc++.h>
+
+std::string urlEncode(const std::string& value);
+
+std::string hexDecode(const std::string& value);
+
+std::string hexEncode(const std::string& input);
+
+bool hasPiece(const std::string& bitField, int index);
+
+void setPiece(std::string& bitField, int index);
+
+int bytesToInt(std::string bytes);
+
+std::string formatTime(long seconds);
+
+#endif
