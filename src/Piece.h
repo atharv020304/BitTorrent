5f0f74f6dc6e3f1d5f1d971531fbe4a7b91b3a38
Atharv Bapat <atharvabapat3@gmail.com>
2026-07-06 18:21:37 +0530

    added piece.h

---
+++ src/Piece.h
@@ -0,0 +1,25 @@
+#ifndef TORRENT_PIECE_H
+#define TORRENT_PIECE_H
+
+#include "Block.h"
+#include <vector>
+
+
+class Piece
+{
+    private:
+        const std::string hVal;
+
+    public:
+        const int index;
+        std::vector<Block*> blk;
+
+        explicit Piece(int index, std::vector<Block*> blk, std::string hVal);
+        std::string getData();
+        Block* nextReq();
+        void blockReceived(int offset, std::string data);
+        bool isComplete();
+        bool isMatchingHash();
+        ~Piece();
+
+}
