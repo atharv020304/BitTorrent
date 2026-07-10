6a7e1280dfa5b45a693a51b6735fb8e7fc7a947a
Atharv Bapat <atharvabapat3@gmail.com>
2026-07-10 20:51:00 +0530

    peer fetching realted fucntions

---
+++ src/RetrivePeers.h
@@ -0,0 +1,29 @@
+#ifndef TORRENT_PEER_RETRIVER_H
+#define TORRENT_PEER_RETRIVER_H
+
+#include<vector>
+#include<cpr/cpr.h>
+
+
+struct Peer
+{
+    std::string ip;
+    int port;
+}
+
+class PeerRetriever
+{
+    private:    
+        std::string announceUrl;
+        std::string infoHash;
+        std::string peerId;
+
+        int port;
+        const unsigned long fileSize;
+        std::vector<Peer*> resp(std::string response);
+    public:
+        explicit PeerFetcher(std::string peerId, std::string announceUrl, std::string infoHash, int port, unsigned long fileSize);
+        std::vector<Peer*> retrivePeers(unsigned long bytesDnld = 0);
+};
+
+#endif /*TORRENT_PEER_RETRIVER_H*/
